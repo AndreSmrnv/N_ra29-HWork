fe2ebd68b6d0ade363298d42e05a46a12f72eb9e
AndreSmrnv <tehmir@bk.ru>
2021-07-20 10:52:21 +0300

    add data

--- 8_hooks-context_1/src/сomponents/Users.jsx
+++ 8_hooks-context_1/src/сomponents/Users.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import UsersList from "./UsersList";
 import UsersCard from "./UsersCard";
-import { INIT_DATA } from '../utils/initData';
+import { INIT_DATA, INIT_ERROR } from '../utils/initData';
+import {  TryAgain } from '../utils/shared/Components';
 
 
 
@@ -10,6 +11,7 @@ export default function Users() {
   const [users, setUsers] = useState(INIT_DATA);
   const [userId, setUserId] = useState(0);
   const [userDetails, setUserDetails] = useState({});
+  const [apiError, setApiError] = useState(INIT_ERROR);
 
   useEffect(() => {
     getData()
@@ -20,18 +22,38 @@ export default function Users() {
   }, [userId]);
 
   const getData = async () => {
+    setApiError(INIT_ERROR);
     await fetch(`${process.env.REACT_APP_URL}/users.json`)
-      .then((response) => response.json())
+      .then(
+        (response) => (response.ok)
+           ? response.json()
+           : Promise.reject(`api err: ${response.status}`)
+      )
       .then((result) => {
         setUsers([...result]);
-      });
+      })
+      .catch(e => {
+        console.log(e);
+        setApiError(prev=> ({...prev, getUserFailed: true, errorMsg: e  }));
+      }) ;
+
+      
   }
   const getUserDetails = async (id) => {
+    setApiError(INIT_ERROR);
     await fetch(`${process.env.REACT_APP_URL}/${id}.json`)
-      .then((response) => response.json())
+      .then(
+        (response) => (response.ok)
+        ? response.json()
+        : Promise.reject(`api err: ${response.status}`)
+      )
       .then((result) => {
         setUserDetails({ ...result });
-      });
+      })
+      .catch(e => {
+        console.log(e);
+        setApiError(prev=> ({...prev, getUserDetailsFailed: true, errorMsg: e  }));
+      }) ;
   }
   const changeUser = (id) => {
     setUserId(id);
@@ -41,11 +63,16 @@ export default function Users() {
   console.log(users);
   console.log(userId);
   console.log(userDetails);
+
+  if (apiError.getUserFailed || apiError.getUserDetailsFailed) {
+    return <TryAgain error={ apiError.errorMsg}/>
+  }
+
   return (
     <>
       <div className="row" >
             <div className="col-4">
-          <UsersList users={users} changeUser={changeUser} userId={ userId}/>
+          <UsersList users={users} changeUser={changeUser} userId={ userId} />
             </div>  
             <div className="col-4">
           {userId &&
