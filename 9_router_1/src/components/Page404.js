1d6304743204ec7b6d20047379d5f02198e3370c
AndreSmrnv <tehmir@bk.ru>
2021-07-21 10:00:17 +0300

    hWork9_1 router/menu

---
+++ 9_router_1/src/components/Page404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page404() {
+    return (
+        <div className="article article__paragraph">
+            Страница не найдена
+        </div>
+    )
+}
